194f79fbbcd605c32342e08678b59607a7745a89
Matt Gill <matt@dustcloudlabs.com>
2020-08-14 19:01:38 -0400

    ex1 breakdown and pseudocode

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,3 +5,6 @@ def get_indices_of_item_weights(weights, length, limit):
     # Your code here
 
     return None
+
+
+    # Find the combination of two elements in weights that equal the limit value, and return tuple containing the index of those two values, where the largest of the two values is at 0 position and smaller is at 1 position 
